1134c1fce36be86462e4062ccbc481555b835ada
nikhilgoel <nikhil.goel.3180@live.com>
2020-05-18 16:29:46 +0530

    * .gitignore: First commit
    * MySuperBank.sln:
    * Program.cs:
    * BankAccount.cs:
    * Transaction.cs:
    * MySuperBank.csproj:
    * AssemblyInfo.cs:

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; set; }
+        public string Owner { get; set; }
+        public decimal Balance { get; set; }
+        private static int accountNumberSeed = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note) { }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note) { }
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var account = new BankAccount("Nikhil", 10000);
+            var account2 = new BankAccount("Suraj", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+            Console.WriteLine($"Account {account2.Number} was created for {account2.Owner} with {account2.Balance}.");
+        }
+    }
+}
