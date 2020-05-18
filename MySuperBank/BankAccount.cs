fb26362716a7128ccece8bb3df63cae4614e0f08
nikhilgoel <nikhil.goel.3180@live.com>
2020-05-18 16:49:34 +0530

    * BankAccount.cs:
    * Program.cs: deposit and withdrawal method implemented

--- MySuperBank/BankAccount.cs
+++ MySuperBank/BankAccount.cs
@@ -7,18 +7,44 @@ namespace MySuperBank
     {
         public string Number { get; set; }
         public string Owner { get; set; }
-        public decimal Balance { get; set; }
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            } }
         private static int accountNumberSeed = 1234567890;
         private List<Transaction> allTransactions = new List<Transaction>();
-        public BankAccount(string name, decimal initialBalance)
+        public BankAccount(string name)
         {
             this.Owner = name;
-            this.Balance = initialBalance;
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
         }
-        public void MakeDeposit(decimal amount, DateTime date, string note) { }
+        public void MakeDeposit(decimal amount, DateTime date, string note) {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
 
-        public void MakeWithdrawal(decimal amount, DateTime date, string note) { }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note) {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if(Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Insufficient balance");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
     }
 }
--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -6,8 +6,8 @@ namespace MySuperBank
     {
         public static void Main(string[] args)
         {
-            var account = new BankAccount("Nikhil", 10000);
-            var account2 = new BankAccount("Suraj", 1000);
+            var account = new BankAccount("Nikhil");
+            var account2 = new BankAccount("Suraj");
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
             Console.WriteLine($"Account {account2.Number} was created for {account2.Owner} with {account2.Balance}.");
         }
